9893816a0d1d1e4b9fd954fc5a9f0e451f90db76
Biren Rao <33708101+birenrao@users.noreply.github.com>
2020-02-10 18:50:11 +0530

    remove unwanted code

--- components/AppContext.tsx
+++ components/AppContext.tsx
@@ -10,17 +10,3 @@ const ctxt = createContext<AppContextInterface | null>(null);
 export const AppContextProvider = ctxt.Provider;
 
 export const AppContextConsumer = ctxt.Consumer;
-
-// export const MovieProviderProps = props => {
-//   const [estimations, setEstimation] = useState([]);
-
-//   const usersContext = {
-//     estimations,
-//     setEstimation,
-//   };
-//   return (
-//     <EstimationContext.Provider value={usersContext}>
-//       {props.children}
-//     </EstimationContext.Provider>
-//   );
-// };
